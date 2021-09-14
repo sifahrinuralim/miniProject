010f89a10dda565f986445365ba59b25d38ab163
Ratu Mutiara <rmpapp8@gmail.com>
2021-09-14 12:02:51 +0700

    initial login

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,9 +4,11 @@ const dataDiriKeluargaUpdateRoute = require('./')
 const dataDiriKeluargaRoute = require('./dataDiriKeluargaRoutes')
 const pengajuanRoute = require('./pengajuanRoute')
 const agunanRoute = require('./agunanRoute')
+const userRoute = require('./userRoute')
 
 router.use('/api/data_diri_keluarga/', dataDiriKeluargaRoute)
 router.use('/api/pengajuan/', pengajuanRoute)
 router.use('/api/agunan/', agunanRoute)
+router.use('/api/user/', userRoute)
 
 module.exports = router
