086bbaa6cf6c27c1d227803b4563e9030c1115cf
Ratu Mutiara <rmpapp8@gmail.com>
2021-09-15 21:45:47 +0700

    Isnasabah and user complete CRUD

--- server/routes/isNasabahRoutes.js
+++ server/routes/isNasabahRoutes.js
@@ -2,5 +2,8 @@ const router = require('express').Router()
 const isnasabahController = require('../controller/isnasabahController')
 
 router.post('/add_form', isnasabahController.isNasabah)
+router.get('/getOne/:id', isnasabahController.getOne)
+router.put('/update/:id',isnasabahController.update)
+router.delete('/delete/:id',isnasabahController.delete)
 
 module.exports = router
