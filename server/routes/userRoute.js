84dfd929f118cbd66d539232b8e6569bd69f8532
Ratu Mutiara <rmpapp8@gmail.com>
2021-09-17 09:19:18 +0700

    goodbye BUG

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -6,10 +6,10 @@ const auth = require('../middleware/auth')
 
 router.post('/daftar', validator.daftar, userController.daftar)
 router.post('/masuk', validator.masuk, userController.masuk)
-router.get('/getAll',  auth, userController.getAll)                         //ngebug
-router.get('/getOne/:id',  auth, validator.checkId, userController.getOne) //ngebug
-router.put('/update/:id',  auth,validator.checkId, userController.update) //ngebug
-router.delete('/delete/:id', auth,validator.checkId, userController.delete) //ngebug
+router.get('/getAll',  auth, userController.getAll)                         //alhamdulilaaaaah bisaa cek lewat headers Authorization
+router.get('/getOne/:id',  auth, validator.checkId, userController.getOne) //bisaa cek lewat headers Authorization
+router.put('/update/:id',  auth,validator.checkId, userController.update) //bisaa cek lewat headers Authorization
+router.delete('/delete/:id', auth,validator.checkId, userController.delete) //bisaa cek lewat headers Authorization
 
 
 module.exports = router
