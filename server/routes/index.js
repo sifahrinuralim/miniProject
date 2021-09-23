3acca1a5007989bc5d8b929f6587b7017cb4523d
Ratu Mutiara <rmpapp8@gmail.com>
2021-09-23 10:41:14 +0700

    fitur upload BE

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,14 +7,23 @@ const dataPerkejaanRoutes = require('./dataPerkejaanRoutes')
 const dataPembiayaanRoutes = require('./dataPembiayaanRoutes')
 const userRoutes = require('./userRoutes')
 const informasiAwalRoutes = require('./informasiAwalRoutes')
+const uploadDocsRoutes = require('./uploadDocsRoutes')
 
+//pengisisan data
 router.use('/api/data_pengajuan/', fasilitasPembiayaanRoutes)
 router.use('/api/data_agunan/', dataAgunanRoutes)
 router.use('/api/data_diri_keluarga/', dataPemohonKeluargaRoutes)
 router.use('/api/data_pekerjaan/', dataPerkejaanRoutes)
 router.use('/api/data_pembiayaan/', dataPembiayaanRoutes)
 
+//landing page
 router.use('/api/user/', userRoutes)
 router.use('/api/isNasabah/', informasiAwalRoutes )
+
+//upload dokumen
+router.use('/api/upload/', uploadDocsRoutes)
+
+//Ringkasan
+
 
 module.exports = router
